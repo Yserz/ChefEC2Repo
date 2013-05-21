e9df9ba154738374c7fd5a496332b2034c653cb3
Yserz <michael.koppen@googlemail.com>
2013-05-21 17:27:54 +0200

    edited appserver.rb

--- roles/appserver.rb
+++ roles/appserver.rb
@@ -4,6 +4,19 @@ description "App server role"
 default_attributes(
   "java" => {
     "install_flavor" => "oracle"
+    "oracle" => {
+      "accept_oracle_download_terms" => true
+    }
+  }
+  "glassfish" => {
+    "domains" => {
+      "domain1" => {
+        "config" => {
+          "admin_port" => 4848
+          "port" => 8080
+        }
+      }
+    }
   }
 )
 all_env = [ 

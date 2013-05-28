000839f1ce3c6df3aed0a7b124e50d66d854aa4b
Yserz <michael.koppen@googlemail.com>
2013-05-28 10:41:50 +0000

    removed sysadmin cookbook from baserole

--- roles/base.rb
+++ roles/base.rb
@@ -1,7 +1,7 @@
 name "base"
 description "Base role applied to all nodes."
+#"recipe[users::sysadmins]",
 run_list(
-  "recipe[users::sysadmins]",
   "recipe[sudo]",
   "recipe[apt]",
   "recipe[git]",

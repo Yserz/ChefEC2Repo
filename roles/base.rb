e28cf2f8833429003a5f8b8e07e2ce3bce8abbf3
Yserz <michael.koppen@googlemail.com>
2013-05-28 12:53:57 +0000

    configured /etc/sudoers

--- roles/base.rb
+++ roles/base.rb
@@ -1,25 +1,23 @@
 name "base"
 description "Base role applied to all nodes."
-#"recipe[users::sysadmins]",
+#
 run_list(
+  "recipe[users::sysadmins]",
   "recipe[sudo]",
   "recipe[apt]",
   "recipe[git]",
   "recipe[build-essential]",
   "recipe[vim]"
 ) 
+## Configure /etc/sudoers
 override_attributes(
   :authorization => {
     :sudo => {
-      :users => ["ubuntu", "vagrant"],
-      :passwordless => true
-    }
-  }
-)
-override_attributes(
-  :java => {
-    :oracle => {
-      "accept_oracle_download_terms" => true
+      #:groups => ["sysadmin", "wheel", "admin", "glassfish-admin"],
+      #:users => ["ubuntu"],
+      #:passwordless => true,
+      :sudoers_defaults => ['env_reset', 'secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'],
+      :include_sudoers_d => true
     }
   }
 )
